9e9b79e89c7a0c6c0f39b7245e92b6603384f3bc
Cidar Parra Merida <cid234.cp@gmail.com>
2023-11-15 02:48:54 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+	$usuario = "usrGuardianPro"; //usuario bd
+	$password = "Pr0dem2017"; //password bd
+	$host= "LOCALHOST"; //"localhost";
+	$bdName = "GuardianPro"; //nombre bd dsn
+?>
---
+++ configsec.php
@@ -0,0 +1,6 @@
+<?php
+	$Susuario = "sa"; //"UsDoc"; //usuario bd
+	$Spassword = "123456"; //password bd
+	$Shost= "localhost"; //"localhost";
+	$SbdName = "sec"; //nombre bd dsn
+?>
